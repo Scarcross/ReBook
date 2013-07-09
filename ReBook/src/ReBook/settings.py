37c4f368387105f39c3d0bc9e7547b983d929916
RicherMans <heini89@gmail.com>
2013-07-09 21:19:35 +0200

    fixed ,

--- ReBook/src/ReBook/settings.py
+++ ReBook/src/ReBook/settings.py
@@ -129,7 +129,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'bookshop',
-    'userauth'
+    'userauth',
 
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
