12760790797471b0b4505dea89ec70bd92a0d864
leip <leip@sas-ws-leip01.swiss-as.com>
2013-07-15 16:34:19 +0200

    booklist working; cut description after 150 characters

--- ReBook/src/bookshop/views.py
+++ ReBook/src/bookshop/views.py
@@ -1,5 +1,5 @@
 # Create your views here.
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response ,render
 from bookshop.models import Book
 
 
@@ -10,7 +10,9 @@ def index(request):
 def booklist(request):
     booklist = Book.objects.all();
     for book in booklist:
+        if len(str(book.description)) > 20:
+            book.description = book.description[:150]+'...'
         print(book.title)
     if booklist is None:
         booklist = 'keine Eintraege vorhanden'
-    return render_to_response('bookshop/booklist.html', {'booklist', 'sdaosidassa'})
+    return render(request, 'bookshop/booklist.html', {'books': booklist})
