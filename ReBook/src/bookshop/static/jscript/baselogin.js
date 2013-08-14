2a62c9769f0d8f01073a3ff6836e1811d6796336
RicherMans <heini89@googlemail.com>
2013-08-14 16:11:21 +0200

    Login screen now correctly redirects to the last failed element

--- ReBook/src/bookshop/static/jscript/baselogin.js
+++ ReBook/src/bookshop/static/jscript/baselogin.js
@@ -3,14 +3,29 @@ $(document)
 				(function($) {
 					// constants
 					var SHOW_CLASS = 'show', HIDE_CLASS = 'hide', ACTIVE_CLASS = 'active';
+
+					$('.tabs').ready(
+							function() {
+								//get the current tab
+								var $curtab = $(this).find("[href='"+js_data +"']");
+								//remove the current active element
+								$('.active').removeClass(ACTIVE_CLASS);
+								
+								//switch current tab to be active
+								$curtab.addClass(ACTIVE_CLASS);
+								$('.show').removeClass(SHOW_CLASS).addClass(
+										HIDE_CLASS);
+								$(js_data).removeClass(HIDE_CLASS).addClass(
+										SHOW_CLASS);
+							});
+
 					$('.tabs').on(
 							'click',
 							'li a',
 							function(e) {
 								e.preventDefault();
-								var $tab = $(this), 
-								href = $tab.attr('href');
-
+								var $tab = $(this), href = $tab.attr('href');
+								console.log($tab);
 								$('.active').removeClass(ACTIVE_CLASS);
 								$tab.addClass(ACTIVE_CLASS);
 
@@ -20,4 +35,6 @@ $(document)
 								$(href).removeClass(HIDE_CLASS).addClass(
 										SHOW_CLASS).hide().fadeIn(500);
 							});
+
 				}));
+
--- ReBook/src/userauth/views.py
+++ ReBook/src/userauth/views.py
@@ -6,8 +6,11 @@ from django.contrib.auth import authenticate
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseForbidden
 from django.contrib.auth import login
+from django.utils import simplejson
 
 def register(request):
+    #Js data is just for redirect to the right anchor after a failure while logging in
+    js_data = simplejson.dumps('#register')
     if request.method == 'POST':  # If the form has been submitted...
         form = RegisterForm(request.POST)
         if(form.is_valid()):
@@ -18,10 +21,12 @@ def register(request):
     else:
         form = RegisterForm()
     loginform = LoginForm()
-    return render(request, 'registration/login.html', {'pwform':form,'form':loginform})
+    return render(request, 'registration/login.html', {'pwform':form,'form':loginform,'js_data':js_data})
 
 def login(request):
     pwform = RegisterForm()
+     #Js data is just for redirect to the right anchor after a failure while logging in
+    js_data = simplejson.dumps('#login')
     if (request.method == 'POST'):
         form = LoginForm(request.POST)
         if(form.is_valid()):
@@ -35,16 +40,16 @@ def login(request):
                     return redirect('thanks')
                     # Redirect to a success page.
                 else:
-                    form.username.errors['loginerror'] += 'you did shit !'
-                    return render(request,'registration/login.html',{'form':form,'pwform':pwform})
+                    form.username.errors['loginerror'] += 'Login err !'
+                    return render(request,'registration/login.html',{'form':form,'pwform':pwform,'js_data':js_data})
                     # Return a 'disabled account' error message
             else:
                 form.errors['othererror'] = ' Other Error !'
-                return render(request,'registration/login.html',{'form':form,'pwform':pwform})
+                return render(request,'registration/login.html',{'form':form,'pwform':pwform,'js_data':js_data})
     else:
         form = LoginForm()#  TODO:
         #     return to user message if this login is valid or not
-    return render(request, 'registration/login.html',{'form':form,'pwform':pwform})
+    return render(request, 'registration/login.html',{'form':form,'pwform':pwform,'js_data':js_data})
 
 def logout(request):
     logout(request)
